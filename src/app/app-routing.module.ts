0823938d6d9cb8cd6d849b18d8f08719dbfd2680
adithgs <adithgs111@gmail.com>
2024-03-01 17:07:10 +0530

    initialised

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'student-list',
+    loadChildren: () => import('./student-list/student-list.module').then( m => m.StudentListPageModule)
+  },
 ];
 
 @NgModule({
